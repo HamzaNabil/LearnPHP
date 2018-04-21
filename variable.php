6aeba1607e3086214765a6864e68169e18896896
HamzaNabil <hamzanabil913@gmail.com>
2018-04-21 17:26:18 +0200

    Learn PHP , Variable

---
+++ variable.php
@@ -0,0 +1,17 @@
+<?php
+    // define variable $test = 2;
+    // variable is declared when init value to it 
+    $test = 2 ;
+
+    // variables are case sensitive 
+    $name = "hamza";
+    $Name = "hamza nabil";
+
+    /*
+        - start with $
+        - start with letter or underscore 
+        - not start with number 
+    */
+
+?>
+    
