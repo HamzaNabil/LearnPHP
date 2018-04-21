f971d34992ddc81b5785e09d23f42e059be85cfa
HamzaNabil <hamzanabil913@gmail.com>
2018-04-21 17:22:03 +0200

    Learn PHP , Syntax

---
+++ syntax.php
@@ -0,0 +1,18 @@
+<?php
+    
+    // you can write php code in any where in the file
+    // php file extension is .php
+     
+
+    // PHP keywords not case sensitive 
+    echo "test";
+    Echo "";
+    eCho "";
+
+    // variable case sensitive ;
+    $test = 1 ;
+    $TesT = 2 ;
+
+?>
+
+
