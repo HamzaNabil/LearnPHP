95ca4e5e3d4921b012c9e1dc082f218a93002f3d
HamzaNabil <hamzanabil913@gmail.com>
2018-05-05 15:29:52 +0200

    Hamza , add File

---
+++ Arithmetic_operator.php
@@ -0,0 +1,24 @@
+<?php 
+
+    // All Operators
+    /*
+    ** Arithmetic op
+    ** Assignment op
+    ** Increment op
+    ** Comparison op
+    ** Logical op
+    ** String op
+    ** Array op
+    */
+
+    // + - * / % **
+    $x = 6 ;
+    $y = 2 ;
+
+    echo $x + $y . "<br>";
+    echo $x - $y . "<br>";
+    echo $x * $y . "<br>";
+    echo $x / $y . "<br>";
+    echo $x % $y . "<br>";
+    echo $x ** $y . "<br>";
+    
