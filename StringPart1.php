6d99f1a1f047d0db205a9bf7c7e3e87c42ba73c1
HamzaNabil <hamzanabil913@gmail.com>
2018-07-07 16:00:27 +0200

    string 2

--- StringPart1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-    $str = "Hello world";
-    echo strlen($str) . "<br>";
-    echo str_word_count($str) . "<br>";
-    echo strrev($str) . "<br>";
-    echo strpos($str,'world');
-    
---
+++ StringPart2.php
@@ -0,0 +1,9 @@
+<?php
+
+    $str = "hello php string";
+
+    echo str_replace("sTring","java",$str); // care about letters ( case-sensitive);
+    echo str_ireplace("sTring","java",$str); // dont care
+    
+
+    
