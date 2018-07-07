609a35392977f5890306208e4d1d0213068ced97
HamzaNabil <hamzanabil913@gmail.com>
2018-07-07 16:05:51 +0200

    array 5

---
+++ ArrayPart5.php
@@ -0,0 +1,14 @@
+<?php
+
+    // array_push 
+    // array_pop
+
+//    $arr = array('text1','text2');
+//    array_push($arr,'text3','text4');
+//    print_r($arr);
+
+    $arr = array('text1','text2');
+    array_pop($arr);
+    print_r($arr);
+
+    
